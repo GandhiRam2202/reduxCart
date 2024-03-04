e0ffc0f1134b7aff443ffe07cec3496bf2244b18
GandhiRam2202 <gandhiramparthi@gmail.com>
2024-03-04 08:57:38 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './Redux/Store'; // Assuming your store is configured in store.js
+import store from './Redux/Store'; 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
